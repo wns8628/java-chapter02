d1aab42b382f07f1471a699c0672ccdd96c5db4f
wns8628 <wns8628>
2019-01-02 09:53:17 +0900

    2019/01/02

---
+++ src/paint/ColorPoint.java
@@ -0,0 +1,23 @@
+package paint;
+
+public class ColorPoint extends Point{
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("점 [x="+ getX() + 
+				", y=" + getY() + 
+				", color=" + color +
+				"]을 그렸습니다.");	
+	}
+	
+}
--- src/paint/MainApp.java
+++ src/paint/MainApp.java
@@ -14,6 +14,20 @@ public class MainApp {
 		Point p2 = new Point(50,50);
 		p2.show();
 		p2.show(false);		
+		
+//		ColorPoint p3 = new ColorPoint();
+		Point p3 = new ColorPoint();
+		((ColorPoint)p3).setColor("red"); //이렇게해줘야 Point가 지꺼만참조하는걸 막을수있다? 
+		
+		p3.setX(100);
+		p3.setY(50);
+		p3.show();
+		p3.show(false);
 	}
 
+//	함수만드는이유. 마우스이벤트발생시 이걸부르면되잔아
+	public static void drawPrint(Point point) {
+		point.show();
+	}
+	
 }
